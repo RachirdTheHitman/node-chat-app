ab7da4e5ae0f97dff82e1175c85525947cbc3330
YiZhang <hyacinthzy1@gmail.com>
2018-05-10 15:05:28 +1000

    Add connect and disconnet event handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+// const bodyParser = require('body-parser');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.use(bodyParser.json());
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {                  //app.listen will run http.createServer method behind scenes
   console.log(`Started at port ${port}`);
 });
