e93d2c1f196c656b5054b771002f073e4affde18
YiZhang <hyacinthzy1@gmail.com>
2018-10-18 15:45:58 +1100

    emit newMessage on createMessage

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io();          //   start up a request to connect to server
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -15,6 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessge', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
